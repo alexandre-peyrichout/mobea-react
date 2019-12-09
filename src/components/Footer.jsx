7f6361173224310656bda571ee9344fd5ef54e5d
Alexandre Peyrichout <a.peyrichout@gmail.com>
2019-12-09 16:16:57 +0100

    footer v1 done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Link from '@material-ui/core/Link';
+import { Container, Box } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    '& a': {
+      color: 'grey'
+    },
+    '& a:hover': {
+      textDecoration: 'none'
+    }
+  },
+  boxRight: {
+    marginLeft: 'auto'
+  },
+  boxMiddle: {
+    marginLeft: '5%'
+  },
+  bottomLinks: {
+    padding: '1rem'
+  }
+}));
+
+export default function Footer() {
+  const classes = useStyles();
+
+  return (
+    <Container component="div">
+      <Toolbar className={classes.root}>
+        <Box className={classes.boxLeft}>
+          <Link href="/test">Logo</Link>
+        </Box>
+        <Box className={classes.boxMiddle} />
+        <Link className={classes.bottomLinks} href="/test">
+          CGU
+        </Link>
+        <Link className={classes.bottomLinks} href="/test">
+          FAQ
+        </Link>
+        <Link className={classes.bottomLinks} href="/test">
+          Politique de confidentialité
+        </Link>
+        <Link className={classes.bottomLinks} href="/test">
+          Contact
+        </Link>
+        <Link className={classes.bottomLinks} href="/test">
+          Site
+        </Link>
+        <Box className={classes.boxRight} />
+        <Link href="/test">Copyright OuiExpat</Link>
+      </Toolbar>
+    </Container>
+  );
+}
