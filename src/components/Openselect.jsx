91c811c38ce0b2ee5595b9a7cd10f3eb5c2b9c3f
pierrekzo <pierrecasaux@gmail.com>
2019-12-11 14:14:58 +0100

    flag added

--- src/components/Openselect.jsx
+++ src/components/Openselect.jsx
@@ -7,57 +7,54 @@ import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
-    button: {
-        display: 'block',
-        marginTop: theme.spacing(2),
-    },
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-    },
+  button: {
+    display: 'block',
+    marginTop: theme.spacing(2)
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  }
 }));
 
 export default function Openselect() {
-    const classes = useStyles();
-    const [country, setCountry] = React.useState('');
-    const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+  const [country, setCountry] = React.useState('');
+  const [open, setOpen] = React.useState(false);
 
-    const handleChange = event => {
-        setCountry(event.target.value);
-    };
+  const handleChange = event => {
+    setCountry(event.target.value);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleOpen = () => {
-        setOpen(true);
-    };
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-    return (
-        <div>
-            <Button className={classes.button} onClick={handleOpen}>
-                Open the select
-      </Button>
-            <FormControl className={classes.formControl}>
-                <InputLabel id="demo-controlled-open-select-label">Country</InputLabel>
-                <Select
-                    labelId="demo-controlled-open-select-label"
-                    id="demo-controlled-open-select"
-                    open={open}
-                    onClose={handleClose}
-                    onOpen={handleOpen}
-                    value={country}
-                    onChange={handleChange}
-                >
-                    <MenuItem value="">
-                        <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Morocco</MenuItem>
-                    <MenuItem value={20}>Germany</MenuItem>
-                    <MenuItem value={30}>Tunisia</MenuItem>
-                </Select>
-            </FormControl>
-        </div>
-    );
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-controlled-open-select-label">Country</InputLabel>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={country}
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Morocco</MenuItem>
+          <MenuItem value={20}>Germany</MenuItem>
+          <MenuItem value={30}>Tunisia</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Layout from '../components/Layout';
+import Openselect from '../components/Openselect';
+import Flag from 'react-world-flags'
+
+import flag from '../assets/drapeau-maroc.jpg';
 
 const useStyles = makeStyles(() => ({
   parent: {
@@ -24,7 +28,11 @@ export default function FullWidthGrid() {
     <Layout>
       <div className={classes.parent}>
         <Paper className={classes.div1}>1</Paper>
-        <Paper className={classes.div2}>2</Paper>
+        <Paper className={classes.div2}>
+          <Openselect />
+          <Flag code={504} width="40" />
+          <h3>Tanger</h3>
+        </Paper>
         <Paper className={classes.div3}>3</Paper>
         <Paper className={classes.div4}>4</Paper>
       </div>
