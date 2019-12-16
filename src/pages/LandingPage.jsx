80858380df13b67712cb6480ce3dc28fd54be7c4
psycho33200 <pininfarina47@gmail.com>
2019-12-16 17:39:02 +0100

    landing page ok

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,32 +1,23 @@
 import React from 'react';
 import { Container, Typography, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import fond from '../assets/bgold.jpg';
-import logo from '../assets/logo.png';
 import LandingBar from '../components/LandingBar';
 import CreateOutlinedIcon from '@material-ui/icons/CreateOutlined';
 import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
 import TextsmsOutlinedIcon from '@material-ui/icons/TextsmsOutlined';
 import EventNoteOutlinedIcon from '@material-ui/icons/EventNoteOutlined';
 import PhoneIphoneOutlinedIcon from '@material-ui/icons/PhoneIphoneOutlined';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles(() => ({
+  root: {
+    margin: '0',
+    minWidth: '100%'
+  },
   contain: {
     margin: '0',
     padding: '0',
@@ -47,7 +38,7 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    width: '100%',
+    minWidth: '100%',
     minHeight: '100vh'
   },
 
@@ -81,8 +72,16 @@ const useStyles = makeStyles(() => ({
   expandOpen: {
     transform: 'rotate(180deg)'
   },
-  avatar: {
-    backgroundColor: red[500]
+  frame: {
+    marginBottom: '3%'
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  apropos: {
+    marginBottom: '3%',
+    marginTop: '3%'
   }
 }));
 
@@ -108,8 +107,12 @@ function LandingPage() {
         </Container>
       </Container>
       <Container className={classes.propos}>
-        <Typography variant="h3">A PROPOS</Typography>
+        <Typography className={classes.apropos} variant="h3">
+          A PROPOS
+        </Typography>
         <iframe
+          title="video"
+          className={classes.frame}
           width="800"
           height="450"
           src="https://www.youtube.com/embed/bh7znN9kMS8"
@@ -118,21 +121,19 @@ function LandingPage() {
           allowfullscreen
         ></iframe>
 
-        <Grid container spacing={1}>
-          <Grid container item xs={6} md={2}>
+        <Grid container spacing={1} justify="center" style={{ minHeight: '35vh' }}>
+          <Grid container item xs={6} md={2} justify="center">
             <Card className={classes.card}>
               <CardHeader
-                avatar={
-                  <Avatar aria-label="recipe" className={classes.avatar}>
-                    R
-                  </Avatar>
-                }
-                title="Shrimp and Chorizo Paella"
+                className={classes.header}
+                avatar={<PhoneIphoneOutlinedIcon fontSize="large" />}
+                title="Une application mobile intelligente"
+                align="center"
               />
               <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  This impressive paella is a perfect party dish and a fun meal to cook together
-                  with your guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                <Typography variant="body2" color="textSecondary" component="p" align="center">
+                  Grâce à votre communauté proactive et collaborative : le partage n'aura plus de
+                  secrets pour vous.
                 </Typography>
               </CardContent>
             </Card>
@@ -140,17 +141,14 @@ function LandingPage() {
           <Grid container item xs={6} md={2}>
             <Card className={classes.card}>
               <CardHeader
-                avatar={
-                  <Avatar aria-label="recipe" className={classes.avatar}>
-                    R
-                  </Avatar>
-                }
-                title="Shrimp and Chorizo Paella"
+                className={classes.header}
+                avatar={<ShareOutlinedIcon fontSize="large" />}
+                title="Organisez"
               />
               <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  This impressive paella is a perfect party dish and a fun meal to cook together
-                  with your guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                <Typography variant="body2" color="textSecondary" component="p" align="center">
+                  Recherchez des expatrié.e.s et organisez votre expatriation depuis votre mobile:
+                  votre destination, vos démarches, votre installation...
                 </Typography>
               </CardContent>
             </Card>
@@ -158,17 +156,16 @@ function LandingPage() {
           <Grid container item xs={6} md={2}>
             <Card className={classes.card}>
               <CardHeader
-                avatar={
-                  <Avatar aria-label="recipe" className={classes.avatar}>
-                    R
-                  </Avatar>
-                }
-                title="Shrimp and Chorizo Paella"
+                className={classes.header}
+                avatar={<TextsmsOutlinedIcon fontSize="large" />}
+                title="Des rencontres approuvées"
+                align="center"
               />
               <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  This impressive paella is a perfect party dish and a fun meal to cook together
-                  with your guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                <Typography variant="body2" color="textSecondary" component="p" align="center">
+                  Grâce à notre algoritme, notre application vous propose les amis qui vous
+                  complètent, partagent vos passions et que vous n'auriez probablement jamais
+                  croisés.
                 </Typography>
               </CardContent>
             </Card>
@@ -176,17 +173,15 @@ function LandingPage() {
           <Grid container item xs={6} md={2}>
             <Card className={classes.card}>
               <CardHeader
-                avatar={
-                  <Avatar aria-label="recipe" className={classes.avatar}>
-                    R
-                  </Avatar>
-                }
-                title="Shrimp and Chorizo Paella"
+                className={classes.header}
+                avatar={<EventNoteOutlinedIcon fontSize="large" />}
+                title="Connectez vous avec les autres expatrié(e)s"
+                align="center"
               />
               <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  This impressive paella is a perfect party dish and a fun meal to cook together
-                  with your guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                <Typography variant="body2" color="textSecondary" component="p" align="center">
+                  Restez connéecté.e.s. Suivez et partagez avec les expatrié.e.s puis organisez vos
+                  Meet up selon vos profils et préférences.
                 </Typography>
               </CardContent>
             </Card>
@@ -194,17 +189,15 @@ function LandingPage() {
           <Grid container item xs={6} md={2}>
             <Card className={classes.card}>
               <CardHeader
-                avatar={
-                  <Avatar aria-label="recipe" className={classes.avatar}>
-                    R
-                  </Avatar>
-                }
-                title="Shrimp and Chorizo Paella"
+                className={classes.header}
+                avatar={<CreateOutlinedIcon fontSize="large" />}
+                title="Personnalisez votre application"
+                align="center"
               />
               <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  This impressive paella is a perfect party dish and a fun meal to cook together
-                  with your guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                <Typography variant="body2" color="textSecondary" component="p" align="center">
+                  Partagez vos loisirs, passions et préférences: créez votre profil personnel et
+                  dites-nous en plus à propos de vos expériences d'expatriation et vos modes de vie.
                 </Typography>
               </CardContent>
             </Card>
