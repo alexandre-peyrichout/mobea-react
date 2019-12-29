737ec2a6cd1afc2ccce4f715142c65c2418852d2
Alexandre Peyrichout <a.peyrichout@gmail.com>
2019-12-29 15:46:07 +0100

    checkcard fix

--- src/components/CheckCard.jsx
+++ src/components/CheckCard.jsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowDropDownCircleIcon from '@material-ui/icons/ArrowDropDownCircle';
 import Checkbox from '@material-ui/core/Checkbox';
-import InfoIcon from '@material-ui/icons/Info';
+
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUserOutlined';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
-  card: { width: '100%', maxWidth: '700px', height: '100%' },
-  cardHeader: {
-    backgroundColor: 'lightgrey'
+  card: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(255, 255, 255,0.3)'
+  },
+  flex: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '16px 16px 0px 24px'
   },
   container: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    borderBottom: 'solid 1px lightgrey',
-    padding: '1rem 0'
+    padding: '1rem 0',
+    backgroundColor: 'rgba(255, 255, 255,0.85)',
+    margin: '4px 0px'
   },
-  infos: {
-    marginLeft: 'auto'
+  listTitle: {
+    fontSize: '1.5rem',
+    color: 'white'
+  },
+  icon: {
+    color: 'white'
   }
 }));
 
@@ -31,64 +43,58 @@ export default function CheckCard() {
 
   return (
     <Card className={classes.card}>
-      <CardHeader
-        className={classes.cardHeader}
-        avatar={<VerifiedUserIcon fontSize="large" />}
-        action={
-          <IconButton aria-label="settings">
-            <ArrowDropDownCircleIcon />
-          </IconButton>
-        }
-        title="ASSURANCE"
-      />
-
+      <div className={classes.flex}>
+        <VerifiedUserIcon className={classes.icon} fontSize="medium" />
+        <Typography variant="h3" className={classes.listTitle}>
+          Assurances
+        </Typography>
+        <IconButton>
+          <ArrowDropDownCircleIcon className={classes.listTitle} />
+        </IconButton>
+      </div>
       <CardContent>
-        <div className={classes.container}>
+        <Card className={classes.container}>
           <Checkbox
             checked
             inputProps={{
               'aria-label': 'primary checkbox'
             }}
+            color="primary"
           />
           lorem ipsum dolor
-          <InfoIcon className={classes.infos} color="disabled" />
-        </div>
-        <div className={classes.container}>
+        </Card>
+        <Card className={classes.container}>
           <Checkbox
             inputProps={{
               'aria-label': 'primary checkbox'
             }}
           />
           lorem ipsum dolor
-          <InfoIcon className={classes.infos} color="disabled" />
-        </div>
-        <div className={classes.container}>
+        </Card>
+        <Card className={classes.container}>
           <Checkbox
             inputProps={{
               'aria-label': 'primary checkbox'
             }}
           />
           lorem ipsum dolor
-          <InfoIcon className={classes.infos} color="disabled" />
-        </div>
-        <div className={classes.container}>
+        </Card>
+        <Card className={classes.container}>
           <Checkbox
             inputProps={{
               'aria-label': 'primary checkbox'
             }}
           />
           lorem ipsum dolor
-          <InfoIcon className={classes.infos} color="disabled" />
-        </div>
-        <div className={classes.container}>
+        </Card>
+        <Card className={classes.container}>
           <Checkbox
             inputProps={{
               'aria-label': 'primary checkbox'
             }}
           />
           lorem ipsum dolor
-          <InfoIcon className={classes.infos} color="disabled" />
-        </div>
+        </Card>
       </CardContent>
     </Card>
   );
