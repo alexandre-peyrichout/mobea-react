af74dec17b2d81d66279c59f8ce6f6b7ff863ebd
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-01-02 10:46:51 +0100

    removed redux, add context

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,25 @@ import LandingPage from './pages/LandingPage';
 import Faq from './pages/Faq';
 import Politique from './pages/Politique';
 import Contact from './pages/Contact';
+import Context from './context/Context';
 
 const App = () => (
   <div>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/faq" component={Faq} />
-        <Route path="/politique" component={Politique} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/404" component={NotFound} />
-        <Redirect to="/404" />
-      </Switch>
-    </BrowserRouter>
+    <Context.Provider value={{ test: 'test du state' }}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/faq" component={Faq} />
+          <Route path="/politique" component={Politique} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/404" component={NotFound} />
+          <Redirect to="/404" />
+        </Switch>
+      </BrowserRouter>
+    </Context.Provider>
   </div>
 );
 
